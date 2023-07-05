b800f73c5fd8726813a84a4078fbed35aa1da54f
XiongJW <2476537825@qq.com>
2023-07-05 20:35:54 +0800

    0705_2

--- src/pages/js/me.js
+++ src/pages/js/me.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $.ajaxSetup({
     headers: {
       "Content-Type": "application/json",
@@ -59,10 +59,10 @@ $(document).ready(function(){
   let account = sessionStorage.getItem("account");
   let new_avatar = "";
   let user_info = {
-    sex:"女",
-    age:30,
-    register_time:"20230703",
-    description:"  快乐星球"
+    sex: "女",
+    age: 30,
+    register_time: "20230703",
+    description: "  快乐星球",
   };
   //主函数
   get_per_info();
@@ -72,35 +72,33 @@ $(document).ready(function(){
   AddToMovieCards();
 
   //1.用户头像和昵称初始化
-  function initialize_users(){
-    let avatar = sessionStorage.getItem('avatar');
+  function initialize_users() {
+    let avatar = sessionStorage.getItem("avatar");
     console.log(avatar);
-    if(avatar)
-    {
+    if (avatar) {
       console.log("进入了头像的更新");
-      $(".avatar").attr("src",avatar);
+      $(".avatar").attr("src", avatar);
       $(".account").text(account);
-    }
-    else{
+    } else {
       console.log("avatar为空，使用默认头像");
     }
   }
 
   //2. 请求:获取用户加入的电影列表并展示
   function initialize_movies() {
-
     let data = {
-      "account":account
+      account: account,
     };
     console.log("用户账号:");
     console.log(data);
     $.ajax({
-      url: "http://192.168.159.207:8080/movie/self?" +"account="+account, // 后端提供的电影列表接口
-      type: "GET",
+      url: "http://192.168.159.207:8080/movie/self", // 后端提供的电影列表接口
+      method: "POST",
+      async: false,
       data: JSON.stringify(data),
-      async:false,
       success: function (response) {
         // 请求成功的回调函数，处理返回的电影列表数据
+        console.log(response);
         var re_movies = response.data;
         var temp_movies = [];
 
@@ -165,40 +163,40 @@ $(document).ready(function(){
     movie_id = $(this).attr("mid");
     //存电影id
     sessionStorage.setItem("movie_id", movie_id);
-    console.log("被点击的电影的id:"+movie_id);
+    console.log("被点击的电影的id:" + movie_id);
     //跳转页面
     window.location.href = "movie.html";
   });
 
   //5. 请求:个人信息填充
-  function get_per_info(){
+  function get_per_info() {
     let data = {
-      account:account
-    }
-    console.log("用户详细信息初始化: "+data);
+      account: account,
+    };
+    console.log("用户详细信息初始化: " + data);
     $.ajax({
-      url:"http://192.168.159.207:8080/user/getUserInfo",
-      type:"POST",
+      url: "http://192.168.159.207:8080/user/getUserInfo",
+      type: "POST",
       data: JSON.stringify(data),
-      success:function(res){
+      success: function (res) {
         console.log("用户详细信息返回信息: ");
         console.log(res);
         //赋值操作
         user_info = {
-          sex:res.sex ? res.sex:"还未填写",
-          age:res.age,
-          register_time:res.register_time,
-          description:res.description ? res.description:"还未填写"
+          sex: res.sex ? res.sex : "还未填写",
+          age: res.age,
+          register_time: res.register_time,
+          description: res.description ? res.description : "还未填写",
         };
         show_user_info(user_info);
       },
-      error:function(err){
-        console.log("获取用户详细信息的请求出错"+err);
-      }
-    })
+      error: function (err) {
+        console.log("获取用户详细信息的请求出错" + err);
+      },
+    });
   }
 
-  function show_user_info(info){
+  function show_user_info(info) {
     $(".sex .alterable").text(info.sex);
     $(".age .alterable").text(info.age);
     $(".register_time .alterable").text(info.register_time);
@@ -207,45 +205,43 @@ $(document).ready(function(){
 
   //6. 用户详细信息修改
   //6.0 输入框初始值填充
-  $(edit_info_btn).click(function(){
-    $("#avatar-preview img" ).attr("src",sessionStorage.getItem('avatar'));
+  $(edit_info_btn).click(function () {
+    $("#avatar-preview img").attr("src", sessionStorage.getItem("avatar"));
     $("#edit_info .name input").val(account);
     $("#edit_info #gender-select").val(user_info.sex);
     $("#edit_info .age input").val(user_info.age);
     $("#edit_info textarea").val(user_info.description);
-
-  })
+  });
   //6.1 头像上传
-  $("#avatar-upload input").change(function(){
+  $("#avatar-upload input").change(function () {
     var file = $(this)[0].files[0];
     var reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       new_avatar = e.target.result;
-      $('#avatar-preview img').attr('src', new_avatar);
-    }
+      $("#avatar-preview img").attr("src", new_avatar);
+    };
     reader.readAsDataURL(file);
-  })
+  });
 
   //6.2信息的提交
-  $("#Form").submit(function(e){
-
+  $("#Form").submit(function (e) {
     e.preventDefault(); // 阻止表单默认提交行为
 
     //var form = $(this);
-    var formData = new FormData(this);
+    const formData = new FormData();
 
     //头像
-    var avatarFile = $('#input_avatar')[0].files[0];
-    formData.append('avatar', avatarFile);
+    var avatarFile = $("#input_avatar")[0].files[0];
+    formData.append("avatar", avatarFile);
 
     let new_sex = $("#edit_info #gender-select").val();
     let new_age = $("#input_age").val();
     let new_description = $("#input_intro").val();
-    
-    formData.append('account', account);
-    formData.set('sex', new_sex);
-    formData.set('age', new_age);
-    formData.set('description', new_description);
+
+    formData.append("account", account);
+    formData.set("sex", new_sex);
+    formData.set("age", new_age);
+    formData.set("description", new_description);
 
     // let data={
     //   'account':account,
@@ -261,42 +257,53 @@ $(document).ready(function(){
     // formData.forEach(function(value, key) {
     //   console.log(key + ': ' + value);})
 
+    const form = new FormData();
+    form.append("avatar", [
+      "C:\\Users\\86173\\Pictures\\联想锁屏壁纸\\8586655.jpg",
+    ]);
+    form.append("account", "user8");
+    form.append("sex", "女");
+    form.append("age", "19");
+    form.append("description", "你好");
+
+    form.forEach(function (value, key) {
+      console.log(key + ": " + value);
+    });
 
     $.ajax({
-      url:"http://192.168.159.207:8080/user/updateUserInfo",
-      type: "POST",
-      data: $('form').serialize(),
-      // headers:{
-      //   "content-type": "multipart/form-data"
-      // },
+      url: "http://192.168.159.207:8080/user/updateUserInfo",
+      method: "POST",
+      headers: {},
       processData: false,
-      contentType: false,
-      success:function(res){
-        console.log("后端返回: " + res);
-        if(res.status_code == "200"){
+      contentType: "multipart/form-data",
+      //mimeType: "multipart/form-data",
+      data: form,
+      crossDomain: true,
+      success: function (res) {
+        console.log("后端返回: ");
+        console.log(res);
+        if (res.status_code == "200") {
           sessionStorage.setItem("avatar", new_avatar);
           //面板个人信息的更新
-          initialize_users();//更新头像和昵称
-          user_info.sex=new_sex;
+          initialize_users(); //更新头像和昵称
+          user_info.sex = new_sex;
           user_info.age = new_age;
           user_info.description = new_description;
           show_user_info(user_info);
           alert("修改成功!");
-        }else{
-          alert("账号重复,请重新取个名字吧!");
+        } else {
+          alert("修改失败!");
         }
       },
-      error:function(err){
+      error: function (err) {
         console.log("上传信息失败: ");
-        console.log(err)
-      }
-    })
-
-
-  })
+        console.log(err);
+      },
+    });
+  });
 
   //6.3 用户密码的修改
-  $("#edit_ps submit").click(function(){
+  $("#edit_ps .submit").click(function () {
     //1. 获取输入框的值
     let password = $("#edit_ps #old").val();
     let password1 = $("#edit_ps #new1").val();
@@ -305,31 +312,32 @@ $(document).ready(function(){
     if (password1 != password2) {
       alert("2次密码不一致！");
       return;
-    } 
-    
-    let data={
-      account:account,
-      old_password:password,
-      new_password:password1
     }
 
+    let adddata = {
+      account: "user8",
+      old_password: "password8",
+      new_password: "123",
+    };
+    console.log(adddata);
     $.ajax({
-      url:"修改密码",
-      type:"POST",
-      data: JSON.stringify(data),
-      success:function(res){
+      url: "http://192.168.159.207:8080/user/updateUserPassword",
+      type: "post",
+      data:adddata,
+      success: function (res) {
         console.log("修改密码时返回的数据: " + res);
         alert("密码修改成功");
       },
-      error:function(err){
-        console.log("修改密码请求出错: " + err);
-        alert("密码修改失败");
-      }
-    })
-  })
+      error: function (err) {
+        console.log("修改密码请求出错: ");
+        console.log(err);
+        alert(err);
+      },
+    });
+  });
 
   //7. 返回上一个页面
-  $(".goback").click(function(){
+  $(".goback").click(function () {
     window.history.back();
-  })
-})
+  });
+});
